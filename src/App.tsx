bae37539eb7eb301db58764fa7ae8bafb495a1c8
wesley berto <wesleyberto4@gmail.com>
2024-01-19 13:38:36 -0300

    adicionando a page login

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 //import Navbar from '';
 //import Footer from '';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-//import Login from '';
+import Login from './paginas/login/Login';
 import Contato from './paginas/contato/Contato';
 import Home from './paginas/home/Home';
 
@@ -12,18 +12,18 @@ import Home from './paginas/home/Home';
 function App() {
   return (
     <>
-        <BrowserRouter>
-          {/* <Navbar /> */}
-          <div className='min-h-[80vh]'>
-            <Routes>
-              {/* <Route path="/" element={<Login />} />
-              <Route path="/login" element={<Login />} /> */}
-              <Route path="/contato" element={<Contato />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          {/* <Footer /> */}
-        </BrowserRouter>
+      <BrowserRouter>
+        {/* <Navbar /> */}
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        {/* <Footer /> */}
+      </BrowserRouter>
     </>
   );
 }
